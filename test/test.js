8772cb85861590c2ebd1b9cfa3d323043d6badfe
Jez Higgins <jez@jezuk.co.uk>
2017-04-18 13:25:47 +0100

    Concat tests.  Not quite complete

--- test/test.js
+++ test/test.js
@@ -53,6 +53,12 @@ describe("MangoRange", () => {
 	t("drop(3).take(3)", from(array).drop(3).take(3), [4,5,6]);
     });
 
-
+    describe("concat", () => {
+	// should behave in a similar way to Array.concat
+	t("concat(array)", from(array).concat(array), array.concat(array));
+	t("concat(1,2,3)", from(array).concat(1,2,3), array.concat(1,2,3));
+	t("concat([4,5,6]", from(array).concat([4,5,6]), array.concat([4,5,6]));
+	t("concat('a','b','c',[4,5,6], 'def')", from(array).concat('a','b','c',[4,5,6], 'def'), array.concat('a','b','c',[4,5,6], 'def'));
+    });
 
 });
